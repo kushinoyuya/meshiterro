59b8cb90fc460a136a3f424ed83fa1f64d92e2b9
kushinoyuya <kusshy15059855@gmail.com>
2019-07-16 16:54:09 +0900

    meshiterro

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'c0d87ad96fc2a0341b79c7b2effc8414975e36a8b3e948b48aedecc5e0f428645bc9b4a6f7d79794d4ea13fc378de0b1534d042b811f155352b9b52b3c6d335c'
